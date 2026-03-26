8be303ec0514849a79b65323f4ab3273d277409d
“Flora” <s252794@dtu.dk>
2026-03-26 17:19:19 +0100

    combone storage part in python file

--- Project_01/project_part_A_C.py
+++ Project_01/project_part_A_C.py
@@ -1,7 +1,7 @@
 # %%
-conda install pandas=2.2
+#conda install pandas=2.2
 
-# %%
+#%%
 import pandas as pd
 import pypsa
 
@@ -395,7 +395,112 @@ df_sensitivity = pd.DataFrame(results).set_index("year")
 
 print(df_sensitivity)
 
-# %%
+# %% ====Part C=====
+network
+
+# Storage Costs
+year = 2030
+url = f"https://raw.githubusercontent.com/PyPSA/technology-data/v0.11.0/outputs/costs_{year}.csv"
+costs = pd.read_csv(url, index_col=[0, 1])
+
+costs.loc[costs.unit.str.contains("/kW"), "value"] *= 1e3
+costs.unit = costs.unit.str.replace("/kW", "/MW")
+
+defaults = {
+    "FOM": 0,
+    "VOM": 0,
+    "efficiency": 1,
+    "fuel": 0,
+    "investment": 0,
+    "lifetime": 25,
+    "discount rate": 0.07,
+}
+costs = costs.value.unstack().fillna(defaults)
+
+costs.at["OCGT", "fuel"] = costs.at["gas", "fuel"]
+costs.at["CCGT", "fuel"] = costs.at["gas", "fuel"]
+
+# --- Storage Units Cost Calculation 1---
+
+capital_cost_storage_1=annuity(10, 0.07) * costs.at["battery inverter", "investment"] + 2 * annuity(25, 0.07) * costs.at["battery storage", "investment"]
+capital_cost_storage_1                                                                # ^ this is 2 hours of storage
+
+
+# --- Storage Units Cost Calculation 3---
+
+costs["marginal_cost"] = costs["VOM"] + costs["fuel"] / costs["efficiency"]
+# annuity = costs.apply(lambda x: annuity(x["discount rate"], x["lifetime"]), axis=1)
+# changed discount rate and lifetime order ^
+annuity = costs.apply(lambda x: annuity(x["lifetime"], x["discount rate"]), axis=1)
+
+costs["capital_cost"] = (annuity + costs["FOM"] / 100) * costs["investment"]
+
+capital_cost_storage_3=costs.at["battery inverter", "capital_cost"] + 2 *costs.at["battery storage", "capital_cost"]
+print(capital_cost_storage_3)
+
+# We add another carrier: battery storage
+
+network.add("Carrier", "battery", co2_emissions=0)
+
+# --- Storage Units ---
+network.add(
+   "StorageUnit",
+   "SE storage",
+   bus="electricity bus",
+   carrier="battery storage",
+
+   max_hours=2,
+   capital_cost=capital_cost_storage_3,
+   efficiency_store=costs.at["battery inverter", "efficiency"],
+   efficiency_dispatch=costs.at["battery inverter", "efficiency"],
+   p_nom_extendable=True,
+   cyclic_state_of_charge=True,
+)
+
+network
+
+
+network.optimize(solver_name="gurobi") # Solve the optimization problem with the new storage unit
+# add this if doesnt work , assign_all_duals=False
+
+
+labels = ['onshore wind',
+          'offshore wind',
+          'solar',
+          'nuclear',
+          'gas (OCGT)',
+          'battery storage']
+
+sizes = [network.generators_t.p['onshorewind'].sum(),
+         network.generators_t.p['offshorewind'].sum(),
+         network.generators_t.p['solar'].sum(),
+         network.generators_t.p['nuclear'].sum(),
+         network.generators_t.p['OCGT'].sum(),
+         network.storage_units_t.p['SE storage'].clip(lower=0).sum()]
+
+colors = ['blue', 'green', 'orange', 'purple', 'brown', 'red']
+
+def my_autopct(pct):
+    return f'{pct:.1f}%' if pct > 0 else ''
+
+patches, texts, autotexts = plt.pie(
+    sizes,
+    colors=colors,
+    autopct=my_autopct,
+    textprops={'color': 'white', 'weight': 'bold'},
+    wedgeprops={'linewidth': 0}
+)
+
+plt.axis('equal')
+plt.title('Electricity mix', y=1.07)
+plt.legend(patches, labels, loc="center left", bbox_to_anchor=(1, 0.5))
+
+# Print the capital costs for each technology
+print("",capital_cost_OCGT, "OCGT")
+print(capital_cost_nuclear, "Nuclear")
+print("",capital_cost_solar, "Solar")
+print("",capital_cost_onshorewind, "Onshore Wind")
+print("",capital_cost_storage_3, "Storage 3")
 
 
 
